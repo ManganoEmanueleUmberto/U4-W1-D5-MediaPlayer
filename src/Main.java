17e174f85b4ec4b155361d25eee71dae48e99444
Emanuele Umberto Mangano <emanuelemangano15@gmail.com>
2024-05-24 15:16:07 +0200

    Creation from keyboard added

--- src/Main.java
+++ src/Main.java
@@ -9,29 +9,77 @@ public class Main {
     public static void main(String[] args) {
         boolean exit = false;
         Scanner sc = new Scanner(System.in);
-        int choice = 0;
-        Audio audio1 = new Audio("Audio 1", 5, 30);
-        Video video1 = new Video("Video 1", 1, 2, 5);
-        Audio audio2 = new Audio("Audio 2", 5, 30);
-        Video video2 = new Video("Video 2", 1, 2, 5);
-        Image image1 = new Image("Image1", 10);
-
-        Multimedia[] arrayMultimedia = {audio1, video1, audio2, video2, image1};
+        int choice = 0, choiceAdd = 0, duration = 0, volume = 0, brightness = 0;
+        String title = "";1
+        Multimedia[] arrayMultimedia = new Multimedia[5];
 
         while (!exit) {
 
+            System.out.println("       -----MENÚ----- \n\n Insert a number between 1-4 || 0 to exit\n 1. Add a new Audio\n 2. Add a new Video\n 3. Add a new Image\n 4. List of all media \n\n 0. Exit");
+            choiceAdd = Integer.parseInt(sc.nextLine());
 
-            System.out.println("       -----MENÚ----- \n\nPick the media you wish to play: ");
-            for (int i = 0; i < arrayMultimedia.length; i++) {
-                System.out.println(" " + (i + 1) + " " + arrayMultimedia[i].getTitle());
+            switch (choiceAdd) {
+                case 1:
+                    System.out.println("Enter the title: ");
+                    title = sc.nextLine();
+                    System.out.println("Enter the duration: ");
+                    duration = Integer.parseInt(sc.nextLine());
+                    System.out.println("Set Volume: ");
+                    volume = Integer.parseInt(sc.nextLine());
+                    for (int i = 0; i < arrayMultimedia.length; i++) {
+                        if (arrayMultimedia[i] == null) {
+                            arrayMultimedia[i] = new Audio(title, duration, volume);
+                            break;
+                        }
+
+                    }
+                    break;
+                case 2:
+                    System.out.println("Enter the title: ");
+                    title = sc.nextLine();
+                    System.out.println("Enter the duration: ");
+                    duration = Integer.parseInt(sc.nextLine());
+                    System.out.println("Set brightness: ");
+                    brightness = Integer.parseInt(sc.nextLine());
+                    System.out.println("Set volume: ");
+                    volume = Integer.parseInt(sc.nextLine());
+                    for (int i = 0; i < arrayMultimedia.length; i++) {
+                        if (arrayMultimedia[i] == null) {
+                            arrayMultimedia[i] = new Video(title, duration, brightness, volume);
+                            break;
+                        }
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Enter the title: ");
+                    title = sc.nextLine();
+                    System.out.println("Set brightness: ");
+                    brightness = Integer.parseInt(sc.nextLine());
+                    for (int i = 0; i < arrayMultimedia.length; i++) {
+                        if (arrayMultimedia[i] == null) {
+                            arrayMultimedia[i] = new Image(title, brightness);
+                            break;
+                        }
+                    }
+                    break;
+                case 4:
+                    System.out.println("       -----MENÚ----- \n\nPick the media you wish to play: ");
+                    for (int i = 0; i < arrayMultimedia.length; i++) {
+                        if (arrayMultimedia[i] != null) {
+                            System.out.println(" " + (i + 1) + " " + arrayMultimedia[i].getTitle());
+                        }
+                    }
+                    System.out.println(" \n 0 Exit");
+                    choice = Integer.parseInt(sc.nextLine());
+                    if (choice == 0) exit = true;
+                    else if (arrayMultimedia[choice - 1] instanceof Audio || arrayMultimedia[choice - 1] instanceof Video)
+                        arrayMultimedia[choice - 1].play();
+                    else arrayMultimedia[choice - 1].show();
+                    break;
+                case 0:
+                    exit = true;
             }
-            System.out.println(" \n 0 Exit");
-            choice = Integer.parseInt(sc.nextLine());
-            if (choice == 0) exit = true;
-            else if (arrayMultimedia[choice - 1] instanceof Audio || arrayMultimedia[choice - 1] instanceof Video)
-                arrayMultimedia[choice - 1].play();
-            else arrayMultimedia[choice - 1].show();
-
         }
     }
 }
